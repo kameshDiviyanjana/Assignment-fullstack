b130d6fc3e7f4c0115f8399b7e2d14bd11d72e4b
kameshDiviyanajan <icrox1234@gmail.com>
2026-07-06 15:54:11 +0530

    Fix:add register validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -19,3 +19,11 @@ export const updateTaskSchema = z.object({
 
 })
 
+
+export  const createUserSchema = z.object({
+  firstname: z.string().min(1, "First name is required"),
+  lastname: z.string().min(1, "Last name is required"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
